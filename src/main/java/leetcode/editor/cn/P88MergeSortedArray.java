4c7763584a10849a8e80baebd68ca200757edd35
RiverLee <riverleechina@163.com>
2020-04-25 01:32:49 +0800

    将本地Java项目JavaCode上传至gitHub

---
+++ src/main/java/leetcode/editor/cn/P88MergeSortedArray.java
@@ -0,0 +1,54 @@
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 
+// 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//输出: [1,2,2,3,5,6] 
+// Related Topics 数组 双指针
+
+package leetcode.editor.cn;
+
+//java:合并两个有序数组
+public class P88MergeSortedArray {
+    public static void main(String[] args) {
+        Solution solution = new P88MergeSortedArray().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+//            for (int i = 0; i <= nums2.length; i++) {
+//                if (nums2[n-1] >= nums1[m-1]) {
+//                    nums1[m+n-1] = nums2[n-1];
+//                    n--;
+//                } else {
+//                    nums1[m+n-1] = nums1[m-1];
+//                    m--;
+//                }
+//            }
+            int i = m-1;
+            int j = n-1;
+            int p = m+n-1;
+            while (i >= 0 &&  j>= 0) {
+                nums1[p--] = (nums1[i] < nums2[j]) ? nums2[j--] : nums1[i--];
+            }
+            System.arraycopy(nums2, 0, nums1,0, j + 1);
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    public void foo(int value) {
+        value = 100;
+        System.out.println("value：" +value);
+    }
+
+    public void toFalse(boolean tag) {
+        tag = false;
+        System.out.println(tag);
+    }
+
+    public void foo(String text) {
+        text = "windows";
+        System.out.println(text);
+    }
+
+    public void foo(StringBuilder builder) {
+        builder.append("4");
+    }
+
+    /**
+     * 测试 值传递 和 引用传递
+     * num = 10,经过foo赋值以后,值没变
+     */
+    @Test
+    public void testPass() {
+        // 基本类型
+        int num = 10;
+        foo(num);
+        System.out.println("num：" +num);
+
+        // 基本类型
+        boolean tag = true;
+        toFalse(tag);
+        System.out.println(tag);
+
+        // 没有提供改变自身方法的引用类型
+        String str = "hello";
+        foo(str);
+        System.out.println(str);
+
+        // 提供了改变自身方法的引用类型
+        StringBuilder sb = new StringBuilder("iphone");
+        foo(sb);
+        System.out.println(sb);
+    }
+}
